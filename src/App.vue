3deb293d8becfe8315cac51487e081da966878c2
RebecaMFep <martinezfeliperebeca@gmail.com>
2023-11-23 13:05:34 +0100

    added changes to app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,34 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+// import { RouterLink, RouterView } from 'vue-router'
+// import HelloWorld from './components/HelloWorld.vue'
+import Login from './components/Login.vue'
+
 </script>
 
 <template>
-  <header>
+ <!--  <header>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
+      <HelloWorld msg="You did it!" /> -->
+      <Login></Login>
+   <!--    <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
       </nav>
-    </div>
-  </header>
+    </div> -->
+  <!-- </header> -->
 
   <RouterView />
 </template>
 
 <style scoped>
-header {
+/* header {
   line-height: 1.5;
   max-height: 100vh;
-}
+} -->
 
-.logo {
+<!-- .logo {
   display: block;
   margin: 0 auto 2rem;
 }
@@ -81,5 +83,5 @@ nav a:first-of-type {
     padding: 1rem 0;
     margin-top: 1rem;
   }
-}
+} --> */
 </style>
