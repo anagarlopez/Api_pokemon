e5637ddbec9e1ce269c1be1c8c9ea22e6c9a16f1
Ana Garcia Lopez <angarlopez@hotmail.com>
2023-11-27 12:42:55 +0100

    photos in header

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <script setup>
+
 import { RouterLink, RouterView } from 'vue-router'
+import Header from './components/Header.vue'
 
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
       
