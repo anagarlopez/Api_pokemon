6464853aa34aa88a49a4e9b73e851c99775ebbdb
Eva López <evamlopez1981@gmail.com>
2023-11-29 21:46:08 +0100

    testing pagination

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,101 @@
+
+<script setup>
+  import { RouterLink, RouterView } from 'vue-router';
+  import PokeApi from './views/PokeApi.vue';
+  import { ref, onMounted } from 'vue'
+
+/* const pokemons = ref([])
+const nextUrl = ref('')
+const previousUrl = ref('')
+ */
+
+let container = document.querySelector(".pokemos");
+let selecPage = document.querySelector("#limit");
+let navigation = document.querySelector(".numbers");
+
+const pokeURL = "https://pokeapi.co/api/v2/pokemon";
+let nextLink = "";
+let prevLink = "";
+
+const changePg = (value) => {
+  const newUrl = `${pokeURL}?limit=${value}`;
+  getPokemos(newUrl);
+}
+
+const prev = () => {
+  getPokemos(prevLink);
+}
+
+const next = () => {
+  getPokemos(nextLink);
+}
+
+const getPokemos = (url) => {
+  fetch(url)
+    .then(response => response.json)
+    .then(responseJson => {
+      console.log(responseJson);
+      nextLink = responseJson.next;
+      prevLink = responseJson.previous;      
+      showPokemons(responseJson.results);
+    })
+}
+const showPokemons = (array) => {
+  array.map(item => {
+    fetch(item.url)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        loadCard(data);
+      })
+  })
+}
+
+const loadCard = (data) => {
+  const imagen = data.sprites.other.home.front_default;
+  const name = data.name;
+
+  let card = document.createElement("div");
+  let content =`
+  <img src="${imagen}" alt="${name}">
+    <p>${name}</p>
+    <p>${data.order}</p>
+  `;
+  card.innerHTML = content;
+  container.appendChild(card);
+}
+
+getPokemos(`${pokeURL}`)
+
+/* 
+// CÓDIGO BORJA
+const updatePokemons = async (url) => {
+  if (url) {
+    const res = await fetch(url)
+    const data = await res.json()
+    nextUrl.value = data.next
+    previousUrl.value = data.previous
+    pokemons.value = await Promise.all(data.results.map(async pokemon => {
+      const pokemonRes = await fetch(pokemon.url)
+      const pokemonData = await pokemonRes.json()
+      return {
+        id: pokemonData.id,
+        name: pokemonData.name,
+        image: pokemonData.sprites.front_default,
+        type: pokemonData.types[0].type.name
+      }
+    }))
+  }
+}
+
+onMounted(() => {
+  updatePokemons("https://pokeapi.co/api/v2/pokemon?limit=10")
+}) */
+
+</script>
+
 <!-- App.vue -->
-<template>
+<!-- <template>
   <header>
     <div id="app">
     <h1>Pokedex</h1>
@@ -29,41 +125,39 @@
           </nav>
       </div>
   </header>
-  <RouterView />
+  <RouterView /> 
+</template> -->
+
+<template>
+  <header>
+    <section>
+      <div class="container">
+        <div class="pokemons">
+          
+        </div>
+      </div>
+
+    </section>
+  </header>
+  
+    
+    <footer>
+      <div class="limit">
+        <p>Por página</p>
+        <select id="limit" onchange="changePg(value);">
+          <option value="40">10</option>
+          <option value="40">20</option>
+          <option value="40">30</option>
+        </select>
+      </div>
+      <div class="navigation">
+        <button onclick="prev()"> &#171; </button>
+        <button onclick="next()"> &#187; </button>
+
+      </div>
+    </footer>
+  
 </template>
-
-<script setup>
-  import { RouterLink, RouterView } from 'vue-router';
-  import PokeApi from './views/PokeApi.vue';
-  import { ref, onMounted } from 'vue'
-
-const pokemons = ref([])
-const nextUrl = ref('')
-const previousUrl = ref('')
-
-const updatePokemons = async (url) => {
-  if (url) {
-    const res = await fetch(url)
-    const data = await res.json()
-    nextUrl.value = data.next
-    previousUrl.value = data.previous
-    pokemons.value = await Promise.all(data.results.map(async pokemon => {
-      const pokemonRes = await fetch(pokemon.url)
-      const pokemonData = await pokemonRes.json()
-      return {
-        id: pokemonData.id,
-        name: pokemonData.name,
-        image: pokemonData.sprites.front_default,
-        type: pokemonData.types[0].type.name
-      }
-    }))
-  }
-}
-
-onMounted(() => {
-  updatePokemons("https://pokeapi.co/api/v2/pokemon?limit=10")
-})
-</script>
 
 <style lang="scss" scoped>
 .pokemon{
