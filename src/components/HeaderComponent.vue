483caebf31591079af0cb3c822d9e75da28485c7
Ana Garcia Lopez <angarlopez@hotmail.com>
2023-12-06 19:44:23 +0100

    changes

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -5,13 +5,10 @@
 <template>
 
   <header>
-
-    <div class="wrapper">
       <nav>
         <img src="./../assets/photos/" alt="Logo_pokemon">
-</nav>
+  </nav>
      
-</div>
 </header>
 
 </template>
@@ -45,6 +42,36 @@ img {
     width: 30%;
     margin-right: 30%;
 }
+}
+*{
+  background-image: url("./assets/photos/Fondo_poke_4.jpeg");
+  background-size: cover;
+  background-repeat: no-repeat;
+  height: 100vh;
+  font-family: 'Jockey One', sans-serif;
+}
+
+main{
+  display: grid;
+  align-items: center;
+  justify-content: center;
+}
+
+nav{
+    font-size: 2rem;
+    
+    a{
+        text-decoration: none;
+        color: black;
+    }
+}
+.nav-icon{
+    padding: 1%;
+    background-color: #B1230F;
+    color: black;
+}
+#logo-pokemon{
+    float: right;
 }
 
 @media only screen and (min-width: 768px) {
