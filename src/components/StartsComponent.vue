76fd4327116c5381eb66b4574ac80e24933d8542
RebecaMFep <martinezfeliperebeca@gmail.com>
2023-12-12 12:55:55 +0100

    added changes

--- src/components/StartsComponent.vue
+++ src/components/StartsComponent.vue
@@ -3,23 +3,21 @@
 
 </script>
 
-
-<template>
-<form>
-  <p class="clasificacion">
-    <input id="radio1" type="radio" name="estrellas" value="5"><!--
-    --><label for="radio1">★</label><!--
-    --><input id="radio2" type="radio" name="estrellas" value="4"><!--
-    --><label for="radio2">★</label><!--
-    --><input id="radio3" type="radio" name="estrellas" value="3"><!--
-    --><label for="radio3">★</label><!--
-    --><input id="radio4" type="radio" name="estrellas" value="2"><!--
-    --><label for="radio4">★</label><!--
-    --><input id="radio5" type="radio" name="estrellas" value="1"><!--
-    --><label for="radio5">★</label>
-  </p>
-</form>
-
+  <template>
+    <form>
+      <p class="clasification">
+        <input id="radio1" type="radio" name="stars" value="5"><!--
+    --> <label for="radio1">★</label><!--
+    --> <input id="radio2" type="radio" name="stars" value="4"><!--
+    --> <label for="radio2">★</label><!--
+    --> <input id="radio3" type="radio" name="stars" value="3"><!--
+    --> <label for="radio3">★</label><!--
+    --> <input id="radio4" type="radio" name="stars" value="2"><!--
+    --> <label for="radio4">★</label><!--
+    --> <input id="radio5" type="radio" name="stars" value="1"><!--
+    --> <label for="radio5">★</label>
+      </p>
+    </form>
 </template>
 
 <style lang="scss" scoped>
@@ -46,7 +44,7 @@ label {
   color: grey;
 }
 
-.clasificacion {
+.clasification {
   direction: rtl;
   unicode-bidi: bidi-override;
 }
